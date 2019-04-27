62cbd678386173a4ebed97b78698b82760ea7575
Annia Forney <anniaforney@gmail.com>
2019-04-27 20:55:35 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,22 @@
 def starts_with_a_vowel?(word)
-  word.match(/[aeiou]/) 
+  word.match(/[aeiou]/) do
   if word.start_with?(/[aeiou]/) == true 
-    else puts false 
   end 
 end 
 
-def words_starting_with_un_and_ending_with_ing(text)
+#def words_starting_with_un_and_ending_with_ing(text)
 
-end
+#end
 
-def words_five_letters_long(text)
+#def words_five_letters_long(text)
 
-end
+#end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+#def first_word_capitalized_and_ends_with_punctuation?(text)
 
-end
+#end
 
-def valid_phone_number?(phone)
-end
+#def valid_phone_number?(phone)
+#end
 
-end 
+#end 
