b99f91c66b4f5702e3f5547d8d88506d5868f65b
Annia Forney <anniaforney@gmail.com>
2019-04-27 23:44:04 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,22 +1,23 @@
 def starts_with_a_vowel?(word)
-  do word.match(/[aeiou]/)
-  if word.start_with?(/[aeiou]/) == true 
+  if word.match (/[aeiou]/)
+end
+
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan (/un+\w+ing)
+end
+
+def words_five_letters_long(text)
+
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+
+end
+
+def valid_phone_number?(phone)
+  if phone.match
+    true
+  else
+    false
   end 
-end 
-
-#def words_starting_with_un_and_ending_with_ing(text)
-
-#end
-
-#def words_five_letters_long(text)
-
-#end
-
-#def first_word_capitalized_and_ends_with_punctuation?(text)
-
-#end
-
-#def valid_phone_number?(phone)
-#end
-
-#end 
+end
