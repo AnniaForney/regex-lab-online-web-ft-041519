926641d847dc17f19493771e04555caf9b84d89f
Annia Forney <anniaforney@gmail.com>
2019-04-27 20:53:30 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match(/[aeiou]/)
-end
+  word.match(/[aeiou]/) 
+  if word.start_with?(/[aeiou]/) == true 
+    else puts false 
+  end 
+end 
 
 def words_starting_with_un_and_ending_with_ing(text)
 
