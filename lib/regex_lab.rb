da139aae5c239ce4eb4bdc3f9cc2b9c843b34edc
Annia Forney <anniaforney@gmail.com>
2019-04-28 00:32:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,14 @@
 def starts_with_a_vowel?(word)
-  if word.match (/[aeiou]/)
+  if word.match (/[aeiouAEIOU]/) 
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan (/un+\w+ing)
+
 end
 
 def words_five_letters_long(text)
-
+text.scan 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,9 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match
-    true
-  else
-    false
-  end 
+  phone.match(/\W (\d+)/) == true 
 end
