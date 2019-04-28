ca817fd99f8df6f9d9a01709170107e02c15d94f
Annia Forney <anniaforney@gmail.com>
2019-04-28 00:38:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\W (\d+)/).count 
+  if phone.match(/\W (\d+)/) 
   return true
 else
   return false
