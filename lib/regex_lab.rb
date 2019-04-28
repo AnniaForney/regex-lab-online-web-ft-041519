bf4d982502853cbb0020e02a10b372990a1d7dc2
Annia Forney <anniaforney@gmail.com>
2019-04-28 00:33:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,4 @@
 def starts_with_a_vowel?(word)
-  if word.match (/[aeiouAEIOU]/)
 
 end
 
@@ -8,7 +7,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-text.scan
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
